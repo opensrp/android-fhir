722ca4d9a9c13966047440cf8f4cf14e677fbc2a
Martin Ndegwa <mndegwa@ona.io>
2023-05-10 12:45:02 +0300

    Release with Aditya's Fwd Include

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -50,7 +50,7 @@ object Releases {
 
   object Engine : LibraryArtifact {
     override val artifactId = "engine"
-    override val version = "0.1.0-beta03-preview2.1-SNAPSHOT"
+    override val version = "0.1.0-beta03-preview2.2-SNAPSHOT"
     override val name = "Android FHIR Engine Library"
   }
 
