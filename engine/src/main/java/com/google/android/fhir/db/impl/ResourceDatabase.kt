7d856eb11f4db2415d30db32b82dc28760309f33
Elly Kitoto <junkmailstoelly@gmail.com>
2023-09-18 17:26:54 +0300

    Resolve database migration MIGRATION_3_4  issue
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- engine/src/main/java/com/google/android/fhir/db/impl/ResourceDatabase.kt
+++ engine/src/main/java/com/google/android/fhir/db/impl/ResourceDatabase.kt
@@ -104,7 +104,20 @@ val MIGRATION_4_5 =
   object : Migration(/* startVersion = */ 4, /* endVersion = */ 5) {
     override fun migrate(database: SupportSQLiteDatabase) {
       database.execSQL(
+        "CREATE INDEX IF NOT EXISTS `index_DateTimeIndexEntity_resourceType_index_name_resourceUuid_index_from_index_to` ON `DateTimeIndexEntity` (`resourceType`, `index_name`, `resourceUuid`, `index_from`, `index_to`)"
+      )
+      database.execSQL(
+        "CREATE INDEX IF NOT EXISTS `index_DateIndexEntity_resourceType_index_name_resourceUuid_index_from_index_to` ON `DateIndexEntity` (`resourceType`, `index_name`, `resourceUuid`, `index_from`, `index_to`)"
+      )
+      database.execSQL(
         "CREATE INDEX IF NOT EXISTS `index_TokenIndexEntity_resourceType_index_name_index_system_index_value_resourceUuid` ON `TokenIndexEntity` (`resourceType`, `index_name`, `index_system`, `index_value`, `resourceUuid`)"
+      )
+      database.execSQL("DROP INDEX IF EXISTS `index_DateTimeIndexEntity_index_from`")
+      database.execSQL(
+        "DROP INDEX IF EXISTS `index_DateTimeIndexEntity_resourceType_index_name_index_from_index_to`"
+      )
+      database.execSQL(
+        "DROP INDEX IF EXISTS `index_DateIndexEntity_resourceType_index_name_index_from_index_to`"
       )
       database.execSQL(
         "DROP INDEX IF EXISTS `index_TokenIndexEntity_resourceType_index_name_index_system_index_value`"
