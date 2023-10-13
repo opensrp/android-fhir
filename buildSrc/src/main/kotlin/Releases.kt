8d2764641a6eaa1f9b3f1b6c84379b5746795e1e
Martin Ndegwa <mndegwa@ona.io>
2023-10-13 14:39:20 +0300

    version bump

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -54,7 +54,7 @@ object Releases {
 
   object DataCapture : LibraryArtifact {
     override val artifactId = "data-capture"
-    override val version = "1.0.0-preview20-SNAPSHOT"
+    override val version = "1.0.0-preview20.1-SNAPSHOT"
     override val name = "Android FHIR Structured Data Capture Library"
   }
 
